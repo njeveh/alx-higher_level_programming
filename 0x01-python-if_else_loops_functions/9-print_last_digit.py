d292a7006e3052c39880fa4b8cfc09f6dd7d088a
njeveh <njeveh@gmail.com>
2022-05-31 20:39:24 +0300

    add task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,4 @@
 def print_last_digit(number):
     """prints the last digit of a number"""
     last_digit = str(number)[-1]
-    print("{}".format(last_digit))
-
-
-print_last_digit(-509)
+    print("{}".format(last_digit), end='')
