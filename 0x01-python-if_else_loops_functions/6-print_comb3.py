ff00182663346ccd5828fdab8a1beb4eca58f904
njeveh <njeveh@gmail.com>
2022-05-31 19:21:57 +0300

    add task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i == j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+            break
+        elif i < j:
+            print("{:d}{:d}".format(i, j), end=',')
