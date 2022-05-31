ced393fb9584b6a1ebe31e6d4a16f8864f7d5968
njeveh <njeveh@gmail.com>
2022-05-31 19:31:41 +0300

    add task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def islower(c):
+    """checks if a character is lowercase or uppercase
+    returns true if lowercase or false if otherwise"""
+    return (ord(c) in range(97, 123))
