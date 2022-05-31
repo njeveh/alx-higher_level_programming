f9b7d6449bc1288577659ca6fedc05b47b2058db
njeveh <njeveh@gmail.com>
2022-05-31 20:30:23 +0300

    add task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     """prints a string in uppercase followed by a new line"""
+    str2 = ''
     for character in str:
         character = ord(character)
         if character in range(97, 123):
             character -= 32
-        print("{0:c}".format(character), end='')
-    print()
+        str2 += chr(character)
+    print("{:s}".format(str2))
