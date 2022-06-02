66ff0e901fc35330def92f6e30320a11251a016d
njeveh <njeveh@gmail.com>
2022-06-03 00:10:36 +0300

    add task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+from add_0 import add
 
 
-if __name__ == '__main__':
-    from add_0 import add
-
+if __name__ == "__main__":
     a = 1
     b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
