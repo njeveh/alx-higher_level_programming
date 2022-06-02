085d6b596ccaf79c7872c10a7f725b9babec8d80
njeveh <njeveh@gmail.com>
2022-06-02 22:46:54 +0300

    add task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import calculator1 as calc
-a = 10
-b = 5
+
 
 if __name__ == "__main__":
+    import calculator1 as calc
+    a = 10
+    b = 5
     print(f"{a} + {b} = {calc.add(a, b)}")
     print(f"{a} - {b} = {calc.sub(a, b)}")
     print(f"{a} * {b} = {calc.mul(a, b)}")
