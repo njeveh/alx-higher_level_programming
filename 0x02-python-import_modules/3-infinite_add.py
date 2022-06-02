db2cca88e231c842a19d6e95092ceacc7f8b5461
njeveh <njeveh@gmail.com>
2022-06-02 23:28:59 +0300

    add task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    argc = len(argv)
+    sum = 0
+
+    for i in range(argc):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
