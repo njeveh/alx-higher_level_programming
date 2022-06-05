b4b04c00e5d471d12819fe57d43277a3ce8ba77e
njeveh <njeveh@gmail.com>
2022-06-06 00:44:15 +0300

    update task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,8 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += 0,
     while len(tuple_b) < 2:
         tuple_b += 0,
-    combined_tuple = zip(tuple_a, tuple_b)
-    my_tuple = ()
-    for tuple_x in combined_tuple:
-        my_tuple += tuple_x[0] + tuple_x[1],
-    return my_tuple
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
