d02cb112e41749972b5621d2e2570b4f86808758
njeveh <njeveh@gmail.com>
2022-06-05 23:28:51 +0300

    ad task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """replaces an element in a list at a specific position
+    without modifying the original list
+    """
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return (new_list)
