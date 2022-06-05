ccd44767305aba0d129e59c3e56f73df006e027b
njeveh <njeveh@gmail.com>
2022-06-06 01:01:59 +0300

    add task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+
+def divisible_by_2(my_list=[]):
+    """ finds all multiples of 2 in a list.
+    """
+    if my_list:
+        boolean_list = [i % 2 == 0 for i in my_list]
+        return boolean_list
