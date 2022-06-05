1e18e94769e923d5ec993ff1542b914e9da97b3a
njeveh <njeveh@gmail.com>
2022-06-05 23:38:49 +0300

    add task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """removes all characters c and C from a string."""
+    if my_string:
+        new_string = my_string[:]
+        new_string.remove(i for i in new_string if i == 'c' or i == 'C')
+        return new_string
