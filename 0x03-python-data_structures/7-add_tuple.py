4075ca7c1a985678a2385fe5928169e24fc4951a
njeveh <njeveh@gmail.com>
2022-06-06 00:33:37 +0300

    add task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """adds two tuples"""
+    while len(tuple_a) < 2:
+        tuple_a += 0,
+    while len(tuple_b) < 2:
+        tuple_b += 0,
+    combined_tuple = zip(tuple_a, tuple_b)
+    my_tuple = ()
+    for tuple_x in combined_tuple:
+        my_tuple += tuple_x[0] + tuple_x[1],
+    return my_tuple
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
