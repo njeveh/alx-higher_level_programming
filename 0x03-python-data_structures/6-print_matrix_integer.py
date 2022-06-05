18980fedb629e88c4d65702d84bda965e7f7e653
njeveh <njeveh@gmail.com>
2022-06-06 00:33:48 +0300

    update task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for i in row:
                 print("{:d}".format(i), end=' ')
-            print()
+            print('$')
+    else:
+        print('$')
