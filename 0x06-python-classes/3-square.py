bc3d2a018901df373d30f93c6f76b10822e7af14
njeveh <njeveh@gmail.com>
2022-06-21 17:37:09 +0300

    add corrections

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
+'''
+Module of a Square class
+'''
 
 
 class Square:
-    """Square class."""
+    '''A simple class that defines a square
+    Attributes:
+        size: The size of the square
+    '''
 
     def __init__(self, size=0):
-        """__init__ method that sets the size of square.
+        '''Initialization method for the Square class
         Args:
-            size (int): size of Square
-        Raises:
-            TypeError: If `size` is not an integer.
-            ValueError: If `size` is less than 0.
-        """
-        if not isinstance(size, int):
+            size(int): The size of the square
+        '''
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
 
     def area(self):
-        """Gets the area of the Square.
-
+        '''Finds the area of the Square
         Returns:
-            Area of squre
-        """
-        return self.__size * self.__size
-
-
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
+            The area of the square
+        '''
+        return self.__size ** 2
