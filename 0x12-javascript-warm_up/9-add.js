32800be42b41a74eaa2e1415cb27369e64dbfbb0
njeveh <njeveh@gmail.com>
2023-08-15 14:26:10 +0300

    add task 9 file

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const num1 = Number(argv[2]);
+const num2 = Number(argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+const sum = add(num1, num2);
+console.log(sum);
