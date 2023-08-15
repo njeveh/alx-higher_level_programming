772fb78ee3f8bf4a24b1af5a6e0832f61661a20b
njeveh <njeveh@gmail.com>
2023-08-15 12:54:34 +0300

    add task six file

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argv = require('process').argv
-let myNumber = argv[2];
+const argv = require('process').argv;
+const myNumber = argv[2];
 if (isNaN(Number(myNumber))) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log(`My number: ${myNumber}`);
+  console.log(`My number: ${myNumber}`);
 }
