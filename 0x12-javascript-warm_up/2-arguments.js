ddd5a7357a5d89a361168eca553f5bdea4a68a25
njeveh <njeveh@gmail.com>
2023-08-15 12:14:38 +0300

    add third task file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const argv = require('process').argv;
-if (argv.length == 2)
-{
+if (argv.length === 2) {
     console.log('No argument');
-}
-else if (argv.length == 3)
-{
+} else if (argv.length === 3) {
     console.log('Argument found')
-}
-else if (argv.length > 3)
-{
+} else if (argv.length > 3) {
     console.log('Arguments found');
 }
