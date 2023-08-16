7e5a01d7249aa78499500104b9fe50508bcfcf4a
njeveh <njeveh@gmail.com>
2023-08-16 23:46:10 +0300

    add task 3 file

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let w;
+    for (let i = this.width; i > 0; --i) {
+      w += 'X';
+    }
+    for (let j = this.height; j > 0; --j) {
+      console.log(w);
+    }
+  }
+}
