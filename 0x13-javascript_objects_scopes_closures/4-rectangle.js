d55ba34f80889aa2e8e9db3b03ef1dde496628d3
njeveh <njeveh@gmail.com>
2023-08-16 23:57:20 +0300

    add task 4 file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let w = '';
+    for (let i = this.width; i > 0; --i) {
+      w += 'X';
+    }
+    for (let j = this.height; j > 0; --j) {
+      console.log(w);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
