39e7bb6ee9512e4fcb4abad780495db75f024bff
njeveh <njeveh@gmail.com>
2023-08-17 00:16:15 +0300

    add task 6 file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Shape = require('./5-square.js');
+class Square extends Shape {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let line = 'C';
+      for (let i = 1; i < this.width; i++) {
+        line = line + 'C';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(line);
+      }
+    }
+  }
+}
+
+module.exports = Square;
