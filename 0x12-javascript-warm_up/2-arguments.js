8b017b346de80b977fa08d81ac7231dc42cbbd39
njeveh <njeveh@gmail.com>
2024-06-17 14:11:36 +0300

    update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const {argv} = require('node:process');
-const numberOfArguments = argv.length;
-if (numberOfArguments === 2) {
+const numberOfArguments = process.argv.length;
+if (numberOfArguments < 3) {
     console.log('No argument');
 } else if (numberOfArguments === 3) {
     console.log('Argument found');
-} else if (numberOfArguments > 3) {
+} else {
     console.log('Arguments found');
 }
