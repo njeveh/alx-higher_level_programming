9643533f58f34f1f36630b92e5119bc778e26aa2
njeveh <njeveh@gmail.com>
2024-06-17 14:37:27 +0300

    update task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
-  
+
+const argv = require('process').argv;
+
+if (!argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
