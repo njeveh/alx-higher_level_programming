834b65793305a24187b8d3a0ae3f7f9eea946edc
njeveh <njeveh@gmail.com>
2024-06-17 17:02:47 +0300

    update task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,10 @@ const size = Number(argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    let line = ''
-    for (let j = 0; j < size; j++) {
-        line += 'X';
+  for (let height = 0; height < size; height++) {
+    let line = '';
+    for (let width = 0; width < size; width++) {
+      line += 'X';
     }
     console.log(line);
   }
