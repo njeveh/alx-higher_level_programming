0d1f2acd8d935181d1148d4b4e81fad8777beaa0
njeveh <njeveh@gmail.com>
2024-06-17 16:47:18 +0300

    update task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arguments = require('process').argv;
-if (isNaN(Number(arguments[2]))) {
+let occurrences = Number(arguments[2]);
+if (isNaN(occurrences)) {
   console.log('Missing number of occurrences');
 } else {
-  let occurrences = Number(arguments[2]);
   while (occurrences > 0) {
     console.log('C is fun');
     --occurrences;
