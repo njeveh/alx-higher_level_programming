d9dc484c908a178a632f41052bfc56ef117ae850
njeveh <njeveh@gmail.com>
2024-06-17 17:50:25 +0300

    update task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
-const argv = require('process').argv;
-const argumentsCount = argv.length;
-const numbers = [];
-if (argumentsCount <= 3) {
+
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
 } else {
-  for (let i = 2; i < argumentsCount; i++) {
-    numbers.push(Number(argv[i]));
-  }
-  numbers.sort();
-  numbers.reverse();
-  console.log(numbers[1]);
+  let fmax = parseInt(process.argv[2]);
+  let nmax = parseInt(process.argv[3]);
+  process.argv.forEach((val, index) => {
+    if (index > 1) {
+      if (fmax < parseInt(val)) {
+        nmax = fmax;
+        fmax = parseInt(val);
+      } else if (parseInt(val) < fmax && parseInt(val) >= nmax) {
+        nmax = parseInt(val);
+      }
+    }
+  });
+  console.log(nmax);
 }
