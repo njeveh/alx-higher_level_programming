8752d58b4612939967f4d0ed0a11990c563c32f7
njeveh <njeveh@gmail.com>
2024-06-19 19:32:18 +0300

    update advanced task 0

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./list-100.js').list;
-
-const newList = list.map((item, index) => item * index);
+let newList = [];
+newList = list.map((item, index) => item * index);
 console.log(list);
 console.log(newList);
