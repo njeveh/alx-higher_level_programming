7ecc59c761a0db8c8d0bcfcc2fa4095d049b1eda
njeveh <njeveh@gmail.com>
2024-06-24 14:36:05 +0300

    update task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
             port='3306',
             user=sys.argv[1],
             passwd=sys.argv[2],
-            db=sys.argv[3]
+            db=sys.argv[3],
+            charset="utf8"
             )
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY states.id ASC")
