94c48f7e873481e8d2d9e5f4a3d5a7e94fbce4fd
njeveh <njeveh@gmail.com>
2024-06-25 01:02:57 +0300

    update task 3 documentation

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-This script takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument
+An SQL injection safe script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 import MySQLdb
 from sys import argv
