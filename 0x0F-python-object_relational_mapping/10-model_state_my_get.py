0c8c99d5a70e5a7fb5b856635b3d33ad4ca2004e
njeveh <njeveh@gmail.com>
2024-06-25 14:29:22 +0300

    update task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == argv[4]).one()
+    state = session.query(State).filter(State.name == argv[4]).first()
     if state is None:
         print('Not found')
     else:
