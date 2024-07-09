edbededd78935872f1826189e292dbe37a9cc5b0
njeveh <njeveh@gmail.com>
2024-07-09 17:57:44 +0300

    update task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET -H "X-School-User-Id:98" "$1"
+curl -sX GET -H "X-School-User-Id: 98" "$1"
