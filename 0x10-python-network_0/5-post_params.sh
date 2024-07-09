fb2abdecef218ee584f563a553554392fbd9c2b7
njeveh <njeveh@gmail.com>
2024-07-09 18:29:47 +0300

    update task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 # A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
