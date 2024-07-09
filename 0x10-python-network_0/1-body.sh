8742c8c07b5ff1e6246bb4f9f1e823467416834a
njeveh <njeveh@gmail.com>
2024-07-09 17:44:44 +0300

    update task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 echo "$(curl -sLI $1)" | grep -q "200 OK" && curl -sL $1
