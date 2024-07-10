fc36d824265c02499f76603eca332ca88173af06
njeveh <njeveh@gmail.com>
2024-07-10 16:49:36 +0300

    add task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,9 @@ import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-    values = {'email' : argv[2]}
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii') # data should be bytes
+    data = data.encode('ascii')  # data should be bytes
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:
         body = response.read()
